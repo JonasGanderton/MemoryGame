b9d3699b62716472ba1e1df73fa6c8620026a833
Jonas <jonasganderton@gmail.com>
2021-05-16 19:56:23 +0100

    Colour changes on mouse hover events

--- src/Card.java
+++ src/Card.java
@@ -1,16 +1,18 @@
 import javafx.scene.control.Button;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.paint.Color;
 
 public class Card extends Button {
+
+    private static final String IDLE_STYLE = "-fx-background-color: #00B299;";
+    private static final String HOVER_STYLE = "-fx-background-color: #009279;";
+
     public Card () {
         super();
     }
 
     public Card(String text) {
         super(text);
-        this.setBackground(new Background(new BackgroundFill(new Color(0, 0.7, 0.6, 0.9), null, getInsets())));
+        setStyle(IDLE_STYLE);
+        setOnMouseEntered(e -> setStyle(HOVER_STYLE));
+        setOnMouseExited(e -> setStyle(IDLE_STYLE));
     }
-
 }
