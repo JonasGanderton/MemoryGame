f7e0f67298549e6a88f5254957033a24d353ac30
Jonas <jonasganderton@gmail.com>
2021-05-20 16:32:53 +0100

    Reformatted, JavaDoc, Card.setPair()

--- src/Card.java
+++ src/Card.java
@@ -1,18 +1,43 @@
 import javafx.scene.control.Button;
 
+/**
+ * Card.java
+ * 
+ * Card used in MemoryApp.
+ * 
+ * @author Jonas Ganderton
+ * @since 15/05/2021
+ */
 public class Card extends Button {
 
     private static final String IDLE_STYLE = "-fx-background-color: #00B299;";
     private static final String HOVER_STYLE = "-fx-background-color: #009279;";
+    
+    private Card pair;
 
-    public Card () {
-        super();
-    }
-
+    /**
+     * Create a card.
+     * 
+     * @param text Text on card.
+     */
     public Card(String text) {
         super(text);
         setStyle(IDLE_STYLE);
         setOnMouseEntered(e -> setStyle(HOVER_STYLE));
         setOnMouseExited(e -> setStyle(IDLE_STYLE));
+    }
+    
+    /**
+     * @return This card's pair.
+     */
+    public Card getPair() {
+        return pair;
+    }
+
+    /**
+     * @param pair This cards's pair to set.
+     */
+    public void setPair(Card pair) {
+        this.pair = pair;
     }
 }
--- src/MemoryApp.java
+++ src/MemoryApp.java
@@ -4,22 +4,34 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
+/**
+ * MemoryApp.java
+ * 
+ * A card matching game developed with JavaFX
+ * 
+ * @author Jonas Ganderton
+ * @since 15/05/2021
+ */
 public class MemoryApp extends Application {
+
+    private static final String BACKGROUND_STYLE = "-fx-background-color: DAE6F3;";
+    private static final int SPACING = 10;
 
     private Pane layout;
     private Scene scene;
-    private static final int SPACING = 10;
-
+    
     public static void main(String[] args) {
         launch(args);
     }
@@ -31,24 +43,28 @@ public class MemoryApp extends Application {
      */
     @Override
     public void start(Stage window) {
-        // Configure the layout
-        layout = addGridPane();
-        try {
-            layout = addTilePane(readCardStrings("cardPairs.txt"));
-        } catch (Exception e) {
-            System.out.println("File not found");
-        }
-
-        layout.setStyle("-fx-background-color: DAE6F3;");
-        layout.setPadding(new Insets(SPACING)); // If all different: ^ > v <
-        
         // Configure the scene and window
+        configureLayout();
         scene = new Scene(layout, 500, 450);
         window.setResizable(false);
         window.setScene(scene);
         window.setTitle("Memory game");
 
         window.show();
+    }
+
+    /**
+     * Configures the layout for the cards.
+     */
+    private void configureLayout() {
+        layout = addGridPane(); // Removes error while I keep both options open.
+        try {
+            layout = addTilePane(readCardStrings("cardPairs.txt"));
+        } catch (Exception e) {
+            System.out.println("File not found");
+        }
+        layout.setStyle(BACKGROUND_STYLE);
+        layout.setPadding(new Insets(SPACING)); // If all different: ^ > v <
     }
 
     /**
@@ -88,21 +104,42 @@ public class MemoryApp extends Application {
             System.out.println(strings[0] + " " + strings[1]);
         }
 
-        //Populate tiles with cards
+        // Populate tiles with cards
         for (String[] cardPair : cardStrings) {
-            for (String cardName : cardPair) {
-                Card c = new Card(cardName);
+            Card c1 = new Card(cardPair[0]);
+            Card c2 = new Card(cardPair[1]);
+            c1.setPair(c2);
+            c2.setPair(c1);
+            tiles.getChildren().addAll(c1,c2);
+        }
+
+        // Make all cards print their name.
+        ObservableList<Node> nodes = tiles.getChildren();
+        for (Node node : nodes) {
+            if (node instanceof Card) {
+                Card c = (Card) node;
                 c.setOnAction(new EventHandler<ActionEvent>() {
                     public void handle(ActionEvent event) {
-                        System.out.println(c.getText()); // Print name on click.
+                        System.out.print(c.getText()); // Print name on click.
                     }
                 });
-                tiles.getChildren().add(c);
+            } else {
+                System.out.println(node.getClass());
             }
         }
         return tiles;
     }
 
+    /**
+     * Reads text from a file.
+     * 
+     * Expected format : <card1a>,<card1b><\n><card2a>,<card2b><\n><...>
+     * Other format (card pairs with self) : <card1><\n><card2><\n><card3><\n><...>
+     * 
+     * @param filename File containing cards.
+     * @return Pairs of strings from the file.
+     * @throws FileNotFoundException File not found.
+     */
     private ArrayList<String[]> readCardStrings (String filename) throws FileNotFoundException {
         ArrayList<String[]> cardPairs = new ArrayList<>();
         File data = new File(filename);
